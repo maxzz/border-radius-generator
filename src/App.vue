38a1844a9dcb625762e47f8fe5a692975b3873aa
Max Zakharzhevskiy <maxzz@msn.com>
2020-12-16 21:15:35 -0800

    feature: corners approach

--- src/App.vue
+++ src/App.vue
@@ -66,6 +66,28 @@
     function random(min, max){
         return Math.floor(min + Math.random() * (max - min));
     }
+    function generateCorners(symmetrical) {
+        let wTL = random(5, 96);
+        let wBL = random(5, 96);
+        let hTL = random(5, 96);
+        let hTR = random(5, 96);
+
+        let wTR, wBR, hBL, hBR;
+
+        if (symmetrical) {
+            wTR = 100 - wTL;
+            wBR = 100 - wBL;
+            hBL = 100 - hTL;
+            hBR = 100 - hTR;
+        } else {
+            wTR = random(5, 96);
+            wBR = random(5, 96);
+            hBL = random(5, 96);
+            hBR = random(5, 96);
+        }
+
+        return { w: [wTL, wTR, wBR, wBL], h: [hTL, hTR, hBR, hBL] };
+    }
 
     function generateShape(symmetrical) {
         let wTL = random(5, 96);
